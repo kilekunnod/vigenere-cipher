90a1f6c717b777f1f4086a9edc53bda6dc426904
Kilekun <kilekunonajide@gmail.com>
2025-05-03 00:49:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 text = 'mrttaqrhknsw ih puggrur'
-custom_key = 'python'
+custom_key = 'python' # could be changed to anything you wish, such as 'happycoding'
 
 def vigenere(message, key, direction=1):
     key_index = 0
